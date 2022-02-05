c474de98b22d1ef0e08cb1754d84a86a8e2696e2
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2022-02-05 18:44:28 +0700

    yep, this too

--- src/main/java/me/hsgamer/bettereconomy/command/sub/GiveSubCommand.java
+++ src/main/java/me/hsgamer/bettereconomy/command/sub/GiveSubCommand.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public class GiveSubCommand extends ChangeMoneySubCommand {
     public GiveSubCommand(BetterEconomy instance) {
-        super(instance, "give", "Give money to the player", "/eco give <player> <amount>");
+        super(instance, "give", "Give money to the player", "/bc give <player> <amount>");
     }
 
     @Override
